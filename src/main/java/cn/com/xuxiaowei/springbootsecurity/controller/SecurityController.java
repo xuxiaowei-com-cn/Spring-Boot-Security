fe388a3a24e25cb73b4bcd7a024a831986d7b44d
徐晓伟 <xuxiaowei@live.hk>
2019-05-18 21:27:18 +0800

    Security Controller

---
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/SecurityController.java
@@ -0,0 +1,28 @@
+package cn.com.xuxiaowei.springbootsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Security Controller
+ *
+ * @author xuxiaowei
+ */
+@Controller
+public class SecurityController {
+
+    /**
+     * 登录页面
+     */
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
+
+        return "login";
+    }
+
+
+}
