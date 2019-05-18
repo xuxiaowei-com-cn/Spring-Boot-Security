5d0e850c06fa0121c09ddcca193b2213715742d9
徐晓伟 <xuxiaowei@live.hk>
2019-05-18 22:57:10 +0800

    Security RestController

---
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/SecurityRestController.java
@@ -0,0 +1,34 @@
+package cn.com.xuxiaowei.springbootsecurity.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Security RestController
+ *
+ * @author xuxiaowei
+ */
+@RestController
+public class SecurityRestController {
+
+    /**
+     * 登录失败
+     */
+    @RequestMapping("/fail.do")
+    public Map<String, Object> fail(HttpServletRequest request, HttpServletResponse response) {
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        data.put("msg", "用户名或密码不正确！");
+
+        return map;
+    }
+
+}
