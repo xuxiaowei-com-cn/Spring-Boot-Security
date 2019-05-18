ca22d03972f365de105ab9d407295cd189bb6af2
徐晓伟 <xuxiaowei@live.hk>
2019-05-18 23:10:08 +0800

    GitHub 图片验证码

---
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/config/ServletConfig.java
@@ -0,0 +1,34 @@
+package cn.com.xuxiaowei.springbootsecurity.config;
+
+import cn.com.xuxiaowei.springbootsecurity.servlet.PatchcaHttpServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Servlet 配置
+ *
+ * @author xuxiaowei
+ */
+@Configuration
+public class ServletConfig {
+
+    /**
+     * 将 PatchcaHttpServlet 注册为 Bean
+     *
+     * @return 可使用 @Autowired 的 Servlet
+     */
+    @Bean
+    PatchcaHttpServlet patchcaHttpServletBean() {
+        return new PatchcaHttpServlet();
+    }
+
+    /**
+     * GitHub 图片验证码 Patchca URL
+     */
+    @Bean
+    ServletRegistrationBean patchcaHttpServlet() {
+        return new ServletRegistrationBean<>(patchcaHttpServletBean(), "/patchca");
+    }
+
+}
---
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/servlet/README.md
@@ -0,0 +1,13 @@
+# cn.com.xuxiaowei.demo.servlet 说明
+
+- 此包的作用？
+
+    - 放置Servlet相关类
+
+- 每个类的作用？
+
+    - PatchcaHttpServlet
+            
+        - GitHub 图片验证码 Patchca
+        
+        - 访问 http://127.0.0.1/patchca 查看图片验证码
