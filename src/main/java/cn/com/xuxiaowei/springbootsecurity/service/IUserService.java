e179e64baa78f54b1fe78c56d0549c155ec24ff7
徐晓伟 <xuxiaowei@live.hk>
2019-05-20 02:29:02 +0800

    根据手机号，查询用户

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/service/IUserService.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/service/IUserService.java
@@ -14,11 +14,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IUserService extends IService<User> {
 
     /**
-     * 根据用户名，查询用户
+     * 根据手机号，查询用户
      *
-     * @param username 用户名，用户名具有唯一性
-     * @return 返回 根据用户名查询的用户（仅有一条数据，多数据报错）
+     * @param phone 手机号，手机号具有唯一性
+     * @return 返回 根据手机号查询的用户（仅有一条数据，多数据报错）
      */
-    User getUsername(String username);
+    User getPhone(String phone);
 
 }
--- src/main/java/cn/com/xuxiaowei/springbootsecurity/service/impl/UserServiceImpl.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/service/impl/UserServiceImpl.java
@@ -19,17 +19,17 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
     /**
-     * 根据用户名，查询用户
+     * 根据手机号，查询用户
      *
-     * @param username 用户名，用户名具有唯一性
-     * @return 返回 根据用户名查询的用户（仅有一条数据，多数据报错）
+     * @param phone 手机号，手机号具有唯一性
+     * @return 返回 根据手机号查询的用户（仅有一条数据，多数据报错）
      */
     @Override
-    public User getUsername(String username) {
+    public User getPhone(String phone) {
 
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
 
-        queryWrapper.eq(User.USERNAME, username);
+        queryWrapper.eq(User.PHONE, phone);
 
         return super.getOne(queryWrapper);
     }
