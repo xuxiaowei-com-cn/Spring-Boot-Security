96f67e67db1ce57395830434123ce3c1d8d25842
徐晓伟 <xuxiaowei@live.hk>
2019-05-19 23:15:02 +0800

    短信登录

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/SmsRestController.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/SmsRestController.java
@@ -1,7 +1,10 @@
 package cn.com.xuxiaowei.springbootsecurity.controller;
 
+import cn.com.xuxiaowei.springbootsecurity.entity.User;
+import cn.com.xuxiaowei.springbootsecurity.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,18 +27,105 @@ import java.util.Map;
 @RequestMapping("/sms")
 public class SmsRestController {
 
+    private final IUserService userService;
+
+    @Autowired
+    public SmsRestController(IUserService userService) {
+        this.userService = userService;
+    }
+
     /**
      * 短信登录
+     *
+     * @param username 手机号
+     * @param patchca  图片验证码
+     * @param smsCode  短信验证码
      */
     @RequestMapping("/login.do")
-    public Map<String, Object> login(HttpServletRequest request, HttpServletResponse response) {
+    public Map<String, Object> login(HttpServletRequest request, HttpServletResponse response,
+                                     String username, String patchca, String smsCode) {
 
         Map<String, Object> map = new HashMap<>(4);
         Map<String, Object> data = new HashMap<>(4);
         map.put("data", data);
 
+        map.put("code", 1);
+
+        if (StringUtils.isEmpty(smsCode)) {
+            map.put("msg", "短信验证码不为空！");
+            return map;
+        }
+
+        if (StringUtils.isEmpty(username)) {
+            map.put("msg", "手机号不为空！");
+            return map;
+        }
+
+        HttpSession session = request.getSession();
+
+        String smsPhoneSession = (String) session.getAttribute("smsPhone");
+
+        if (StringUtils.isEmpty(smsPhoneSession)) {
+            map.put("msg", "用户短信登录受到攻击！");
+            data.put("phone", "Session 中不存在已发送短信验证码的手机号：" + username);
+            return map;
+        }
+
+        if (!username.equals(smsPhoneSession)) {
+            map.put("msg", "手机号验证失败！");
+            return map;
+        }
+
+        LocalDateTime smsCodeTimeSession = (LocalDateTime) session.getAttribute("smsCodeTime");
+
+        if (smsCodeTimeSession == null) {
+            map.put("msg", "用户短信登录受到攻击！");
+            data.put("smsCodeTime", "Session 中不存在短信验证码的创建时间");
+            return map;
+        }
+
+        // 短信验证码的有效时间为 5 分钟
+        LocalDateTime plus = smsCodeTimeSession.plus(5, ChronoUnit.MINUTES);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        if (plus.isBefore(now)) {
+            map.put("msg", "短信验证码过期，请重新获取！");
+            return map;
+        }
+
+        String smsCodeSession = (String) session.getAttribute("smsCode");
+
+        if (StringUtils.isEmpty(smsCodeSession)) {
+            map.put("msg", "用户短信登录受到攻击！");
+            data.put("smsCode", "Session 中不存在发送给" + username + "的短信验证码");
+            return map;
+        }
+
+        if (!smsCode.equals(smsCodeSession)) {
+            map.put("msg", "短信验证码错误！");
+            return map;
+        }
+
+        User user = userService.getUsername(username);
+
+        if (user == null) {
+
+            user = new User().setUsername(username);
+
+            boolean save = userService.save(user);
+
+            if (save) {
+                map.put("msg", "用户创建成功，短信登录成功");
+            } else {
+                map.put("msg", "用户创建失败，短信登录失败！");
+                return map;
+            }
+        } else {
+            map.put("msg", "短信登录成功");
+        }
+
         map.put("code", 0);
-        map.put("msg", "短信登录成功");
 
         return map;
     }
