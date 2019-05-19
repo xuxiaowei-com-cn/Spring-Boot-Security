838bc50a8117a19d7f25f6d233f1fb9e17d193f3
徐晓伟 <xuxiaowei@live.hk>
2019-05-19 13:26:22 +0800

    注册

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/SecurityRestController.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/SecurityRestController.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.springbootsecurity.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +28,21 @@ public class SecurityRestController {
         map.put("data", data);
 
         data.put("msg", "用户名或密码不正确！");
+
+        return map;
+    }
+
+    /**
+     * 注册
+     */
+    @RequestMapping(value = "/reg.do", method = RequestMethod.POST)
+    public Map<String, Object> reg(HttpServletRequest request, HttpServletResponse response) {
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        data.put("msg", "");
 
         return map;
     }
