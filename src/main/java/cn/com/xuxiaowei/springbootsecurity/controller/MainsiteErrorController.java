6d4b708fc02590305b920a04feaeda044847013e
徐晓伟 <xuxiaowei@live.hk>
2019-05-19 20:12:03 +0800

    注释

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/MainsiteErrorController.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/MainsiteErrorController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,8 +18,8 @@ import java.io.IOException;
  * <p>
  * {@link WebServerFactoryCustomizer <>} 与 {@link ErrorController} 各有各的特点，根据自己的需要选择
  * <p>
- * 不保留原始URL：使用 {@link ErrorController}
- * 保留原始URL：使用 {@link WebServerFactoryCustomizer<>}
+ * 自定义请求转发、重定向：使用 {@link ErrorController}
+ * 只能请求转发：使用 {@link WebServerFactoryCustomizer<>}
  * <p>
  * 优先级：{@link WebServerFactoryCustomizer<>} 大于 {@link ErrorController}
  *
@@ -32,7 +33,7 @@ public class MainsiteErrorController implements ErrorController {
      * HttpStatus 状态，参见枚举 {@link HttpStatus}
      */
     @RequestMapping(EEEOR)
-    public void handleError(HttpServletRequest request, HttpServletResponse response) {
+    public void handleError(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         // 获取 HttpStatus 状态，参加枚举 org.springframework.http.HttpStatus
         int statusCode = (int) request.getAttribute("javax.servlet.error.status_code");
@@ -43,18 +44,22 @@ public class MainsiteErrorController implements ErrorController {
         // 302
         int found = HttpStatus.FOUND.value();
 
-        try {
+        if (notFound == statusCode) {
 
-            if (notFound == statusCode) {
-                // 重定向到 404 页面
-                response.sendRedirect("/" + notFound);
-            } else if (found == statusCode) {
-                // 重定向到 302 页面
-                response.sendRedirect("/" + found);
-            }
+            // 重定向到 404 页面
+            // <code>response.sendRedirect("/" + notFound);</code>
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            // 请求转发到 404 页面
+            request.getRequestDispatcher("/" + notFound).forward(request, response);
+
+        } else if (found == statusCode) {
+
+            // 重定向到 302 页面
+            // <code>response.sendRedirect("/" + found);</code>
+
+            // 请求转发到 302
+            request.getRequestDispatcher("/" + found).forward(request, response);
+
         }
 
     }
