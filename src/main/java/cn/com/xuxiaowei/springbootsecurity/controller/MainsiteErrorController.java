0a66d545a4e4851f66305296bc27aa4c8b2a21ed
徐晓伟 <xuxiaowei@live.hk>
2019-05-19 18:55:08 +0800

    主站点错误 Controller

---
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/controller/MainsiteErrorController.java
@@ -0,0 +1,59 @@
+package cn.com.xuxiaowei.springbootsecurity.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 主站点错误 Controller
+ * <p>
+ * 增加 {@link Controller} 注解即可使用
+ *
+ * @author xuxiaowei
+ */
+public class MainsiteErrorController implements ErrorController {
+
+    private final String EEEOR = "/error";
+
+    /**
+     * HttpStatus 状态，参见枚举 {@link HttpStatus}
+     */
+    @RequestMapping(EEEOR)
+    public void handleError(HttpServletRequest request, HttpServletResponse response) {
+
+        // 获取 HttpStatus 状态，参加枚举 org.springframework.http.HttpStatus
+        int statusCode = (int) request.getAttribute("javax.servlet.error.status_code");
+
+        // 404
+        int notFound = HttpStatus.NOT_FOUND.value();
+
+        // 302
+        int found = HttpStatus.FOUND.value();
+
+        try {
+
+            if (notFound == statusCode) {
+                // 重定向到 404 页面
+                response.sendRedirect("/" + notFound);
+            } else if (found == statusCode) {
+                // 重定向到 302 页面
+                response.sendRedirect("/" + found);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public String getErrorPath() {
+        return EEEOR;
+    }
+
+}
