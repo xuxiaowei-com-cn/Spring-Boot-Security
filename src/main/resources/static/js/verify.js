9d4fb087be8c99a5c0677610510e7cc0e9dd374b
徐晓伟 <xuxiaowei@live.hk>
2019-05-19 16:03:18 +0800

    验证 JS

--- src/main/resources/static/js/verify.js
+++ src/main/resources/static/js/verify.js
@@ -5,7 +5,57 @@ layui.define(['form'], function (exports) {
     var $ = layui.$,
         form = layui.form;
 
-    var verify = {};
+    var verify = {
+        /**
+         * 是否包含大写字母
+         */
+        containsUpperCase: function (value) {
+            return new RegExp(/^.*(?=.*[A-Z]).*$/).test(value);
+        },
+
+        /**
+         * 是否包含小写字母
+         */
+        containsLowerCase: function (value) {
+            return new RegExp(/^.*(?=.*[a-z]).*$/).test(value);
+        },
+
+        /**
+         * 是否包含字母
+         */
+        containsCase: function (value) {
+            return new RegExp(/^.*(?=.*[a-zA-Z]).*$/).test(value);
+        },
+
+        /**
+         * 是否包含数字
+         */
+        containsNumber: function (value) {
+            return new RegExp(/^.*(?=.*\d).*$/).test(value);
+        },
+
+        /**
+         * 是否包含特殊字符串
+         */
+        containsSpecial: function (value) {
+            return new RegExp(/^.*(?=.*[~!@#$%^&*()_+-=/:;"'<,>.?\\]).*$/).test(value);
+        },
+
+        /**
+         * 是否包含中文
+         */
+        containsChinese: function (value) {
+            return new RegExp(/^.*(?=.*[\u4e00-\u9fa5]).*$/).test(value);
+        },
+
+        /**
+         * 是否为手机号
+         */
+        isPhone: function (value) {
+            return new RegExp(/^1\d{10}$/).test(value);
+        }
+
+    };
 
     //自定义验证
     form.verify({
@@ -13,9 +63,12 @@ layui.define(['form'], function (exports) {
         /**
          * 用户昵称 验证
          */
-        username: function (value) {
+        phone: function (value) {
             if (value == null || value === "") {
-                return "用户名不能为空！";
+                return "手机号不能为空！";
+            }
+            if (!verify.isPhone(value)) {
+                return "手机号不正确！";
             }
         },
 
@@ -25,6 +78,25 @@ layui.define(['form'], function (exports) {
         password: function (value) {
             if (value == null || value === "") {
                 return "密码不能为空！";
+            }
+            if (!verify.containsUpperCase(value)) {
+                return "密码必须包含大写字母！"
+            }
+            if (!verify.containsLowerCase(value)) {
+                return "密码必须包含小写字母！"
+            }
+            if (!verify.containsNumber(value)) {
+                return "密码必须包含数字！"
+            }
+            if (!verify.containsSpecial) {
+                return "密码必须包含特殊字符！"
+            }
+            if (!verify.containsChinese) {
+                return "密码不能包含中文！"
+            }
+            var length = value.length;
+            if (length < 6 || length > 16) {
+                return "密码长度6-16";
             }
         },
 
