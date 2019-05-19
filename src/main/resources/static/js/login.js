ae471fdc96859597cd0ad55375bc39156090b9a6
徐晓伟 <xuxiaowei@live.hk>
2019-05-19 13:03:42 +0800

    登录

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,20 @@
+/** 登录 JS */
+
+layui.define(['layer'], function (exports) {
+
+    var $ = layui.$,
+        layer = layui.layer,
+        $body = $('body');
+
+    var login = {};
+
+    /**
+     * 更换图形验证码
+     */
+    $body.on('click', '.XXW-patchca-img', function () {
+        this.src = '/patchca?t=' + new Date().getTime()
+    });
+
+    //对外暴露的接口
+    exports('login', login);
+});
