dbc29de79aad7d8cf811a29eee3c5cd208efda3d
徐晓伟 <xuxiaowei@live.hk>
2019-05-21 01:18:20 +0800

    detailsWeChatWebPage

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/servlet/login/WeChatWebPageHttpServlet.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/servlet/login/WeChatWebPageHttpServlet.java
@@ -1,7 +1,9 @@
 package cn.com.xuxiaowei.springbootsecurity.servlet.login;
 
 import cn.com.xuxiaowei.springbootsecurity.setting.SecuritySettings;
+import cn.com.xuxiaowei.springbootsecurity.util.security.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.weixin4j.Weixin;
 import org.weixin4j.component.SnsComponent;
 
@@ -51,6 +53,15 @@ public class WeChatWebPageHttpServlet extends HttpServlet {
         // 官方文档：https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140842
         // 第一步：用户同意授权，获取code
         String oAuth2CodeUserInfoUrl = snsComponent.getOAuth2CodeUrl(domain + securitySettings.weChatWebPageUrl, "snsapi_userinfo", state);
+
+        // 记录远程地址，如果会话已存在（也不会创建会话），还会设置会话ID。
+        // 由于 第三方登录（微信网页（微信内部））授权时，是从第三方页面跳转的进入本站的，故授权时，没有 getDetails()
+        // 第三方登录（微信网页（微信内部））授权时，必须进入此方法
+        WebAuthenticationDetails details = SecurityUtils.getDetails();
+
+        // 在此方法中，将 getDetails() 放入 Session
+        // 授权时，从 Session 中获取
+        session.setAttribute("detailsWeChatWebPage", details);
 
         // 用户同意授权，获取 code ，重定向到微信 URL
         resp.sendRedirect(oAuth2CodeUserInfoUrl);
