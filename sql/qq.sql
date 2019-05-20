c23eeaaf35c6ee360f41e22b7a7c802a0da05acc
徐晓伟 <xuxiaowei@live.hk>
2019-05-20 20:32:49 +0800

    QQ SQL access_token、access_token_expired_date

--- sql/qq.sql
+++ sql/qq.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 20/05/2019 17:22:39
+ Date: 20/05/2019 17:56:57
 */
 
 SET NAMES utf8mb4;
@@ -32,6 +32,8 @@ CREATE TABLE `qq`  (
   `vip` tinyint(1) DEFAULT NULL,
   `level` int(11) DEFAULT NULL,
   `yellow_year_vip` tinyint(1) DEFAULT NULL,
+  `access_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT 'Access Token',
+  `access_token_expired_date` datetime(0) DEFAULT NULL COMMENT 'Access Token 过期时间',
   `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间，自动生成，无需填写并禁止修改。',
   `update_date` datetime(0) DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间，自动生成，无需填写并禁止修改。',
   `deleted` int(1) NOT NULL DEFAULT 0 COMMENT '逻辑删除，0 未删除，1 删除，MySQL 默认值 0，不为 NULL，注解@TableLogic。',
