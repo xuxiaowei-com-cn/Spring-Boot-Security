a245140f4d6ef1369fbeaba7c3956e7664b6fc1d
徐晓伟 <xuxiaowei@live.hk>
2019-05-21 00:26:42 +0800

    处理参数为空、不匹配

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/filter/login/WeChatWebPageAbstractAuthenticationProcessingFilter.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/filter/login/WeChatWebPageAbstractAuthenticationProcessingFilter.java
@@ -1,5 +1,8 @@
 package cn.com.xuxiaowei.springbootsecurity.filter.login;
 
+import cn.com.xuxiaowei.springbootsecurity.util.response.ResponseUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -7,13 +10,17 @@ import org.springframework.security.web.authentication.AbstractAuthenticationPro
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 第三方登录（微信网页（微信内部））认证处理过滤器
  *
  * @author xuxiaowei
  */
+@Slf4j
 public class WeChatWebPageAbstractAuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
 
     public WeChatWebPageAbstractAuthenticationProcessingFilter(String defaultFilterProcessesUrl) {
@@ -22,6 +29,50 @@ public class WeChatWebPageAbstractAuthenticationProcessingFilter extends Abstrac
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        HttpSession session = request.getSession();
+
+        String wechatWebpageConnectState = (String) session.getAttribute("wechat_webpage_connect_state");
+
+        String code = request.getParameter("code");
+
+        String state = request.getParameter("state");
+
+        if (StringUtils.isEmpty(wechatWebpageConnectState)) {
+            map.put("msg", "微信网页（微信内部）登录受到攻击！");
+            data.put("details", "Session 中不存在 微信网页（微信内部） 登录的状态码！");
+            log.debug(map.toString());
+            ResponseUtils.response(response, map);
+            return null;
+        }
+
+        if (StringUtils.isEmpty(code)) {
+            map.put("msg", "微信网页（微信内部）登录受到攻击！");
+            data.put("details", "未收到 微信网页（微信内部） 登录的 code！");
+            log.debug(map.toString());
+            ResponseUtils.response(response, map);
+            return null;
+        }
+
+        if (StringUtils.isEmpty(state)) {
+            map.put("msg", "微信网页（微信内部）登录受到攻击！");
+            data.put("details", "未收到 微信网页（微信内部） 登录的状态码！");
+            log.debug(map.toString());
+            ResponseUtils.response(response, map);
+            return null;
+        }
+
+        if (!state.equals(wechatWebpageConnectState)) {
+            map.put("msg", "微信网页（微信内部）登录受到攻击！");
+            data.put("details", "Session 中的 微信网页（微信内部） 登录的状态码与接收的不同！");
+            log.debug(map.toString());
+            ResponseUtils.response(response, map);
+            return null;
+        }
 
 
         return null;
