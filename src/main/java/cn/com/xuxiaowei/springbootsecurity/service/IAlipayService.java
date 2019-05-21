67931e009bcc53cbc57b82a92ca77a5210a21ca4
徐晓伟 <xuxiaowei@live.hk>
2019-05-21 17:42:46 +0800

    根据 userId（支付宝），查询 Alipay

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/service/IAlipayService.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/service/IAlipayService.java
@@ -13,4 +13,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IAlipayService extends IService<Alipay> {
 
+    /**
+     * 根据 userId（支付宝），查询 Alipay
+     *
+     * @param userId userId（支付宝），userId（支付宝） 具有唯一性
+     * @return 返回 根据 userId（支付宝） 查询的 Alipay（仅有一条数据，多数据报错）
+     */
+    Alipay getUserId(String userId);
+
 }
--- src/main/java/cn/com/xuxiaowei/springbootsecurity/service/impl/AlipayServiceImpl.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/service/impl/AlipayServiceImpl.java
@@ -3,6 +3,7 @@ package cn.com.xuxiaowei.springbootsecurity.service.impl;
 import cn.com.xuxiaowei.springbootsecurity.entity.Alipay;
 import cn.com.xuxiaowei.springbootsecurity.mapper.AlipayMapper;
 import cn.com.xuxiaowei.springbootsecurity.service.IAlipayService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +18,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class AlipayServiceImpl extends ServiceImpl<AlipayMapper, Alipay> implements IAlipayService {
 
+    /**
+     * 根据 userId（支付宝），查询 Alipay
+     *
+     * @param userId userId（支付宝），userId（支付宝） 具有唯一性
+     * @return 返回 根据 userId（支付宝） 查询的 Alipay（仅有一条数据，多数据报错）
+     */
+    @Override
+    public Alipay getUserId(String userId) {
+
+        QueryWrapper<Alipay> queryWrapper = new QueryWrapper<>();
+
+        queryWrapper.eq(Alipay.USER_ID, userId);
+
+        return super.getOne(queryWrapper);
+    }
 }
