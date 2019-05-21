1de8b17f68d09ccd972a08c2acf3171772505df2
徐晓伟 <xuxiaowei@live.hk>
2019-05-21 18:11:49 +0800

    支付宝 配置（类变量范围）

--- src/main/java/cn/com/xuxiaowei/springbootsecurity/setting/AlipaySettings.java
+++ src/main/java/cn/com/xuxiaowei/springbootsecurity/setting/AlipaySettings.java
@@ -17,52 +17,52 @@ public class AlipaySettings {
     /**
      * 域名
      */
-    private String domain;
+    public String domain;
 
     /**
      * 支付宝网关（固定）
      * <p>
      * https://openapi.alipay.com/gateway.do
      */
-    private String url;
+    public String url;
 
     /**
      * APPID 即创建应用后生成
      */
-    private String appId;
+    public String appId;
 
     /**
      * 开发者私钥，由开发者自己生成
      */
-    private String appPrivateKey;
+    public String appPrivateKey;
 
     /**
      * 参数返回格式，只支持json
      * <p>
      * json（固定）
      */
-    private String format;
+    public String format;
 
     /**
      * 编码集，支持GBK/UTF-8
      */
-    private String charset;
+    public String charset;
 
     /**
      * 支付宝公钥，由支付宝生成
      */
-    private String alipayPublicKey;
+    public String alipayPublicKey;
 
     /**
      * 商户生成签名字符串所使用的签名算法类型，目前支持RSA2和RSA，推荐使用RSA2
      * <p>
      * RSA2
      */
-    private String signType;
+    public String signType;
 
     /**
      * 授权 URL（获取 code）
      */
-    private String publicAppAuthorize;
+    public String publicAppAuthorize;
 
 }
