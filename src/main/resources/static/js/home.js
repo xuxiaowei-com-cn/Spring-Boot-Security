a2bb671935d24e9b786882c5837e7a21b6653696
徐晓伟 <xuxiaowei@live.hk>
2019-05-22 20:49:25 +0800

    登录按钮

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -6,6 +6,8 @@ layui.define(['layer'], function (exports) {
         layer = layui.layer,
         $body = $('body');
 
+    var HIDE = "layui-hide";
+
     var home = {};
 
     /**
@@ -14,6 +16,15 @@ layui.define(['layer'], function (exports) {
     $body.on('click', '.XXW-patchca-img', function () {
         this.src = '/patchca?t=' + new Date().getTime()
     });
+
+    // 是微信
+    // 需要引入微信JS（<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>）
+    if (typeof WeixinJSBridge != "undefined") {
+        // 隐藏微信扫码登录
+        $(".XXW-wechat-website").addClass(HIDE);
+        // 显示微信内部登录
+        $(".XXW-wechat-webpage").removeClass(HIDE);
+    }
 
     //对外暴露的接口
     exports('home', home);
