af4695ab5d1cfad663b1cb9ba5faa5aca3734a25
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-08-05 00:07:19 +0800

    升级依赖

--- src/main/java/cn/com/xuxiaowei/security/config/MybatisPlusConfig.java
+++ src/main/java/cn/com/xuxiaowei/security/config/MybatisPlusConfig.java
@@ -1,11 +1,9 @@
 package cn.com.xuxiaowei.security.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -13,15 +11,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * <p>
  * 扫描 Mapper 接口
  * <p>
- * MyBatis Plus 性能分析插件
- * <p>
- * 性能分析拦截器，用于输出每条 SQL 语句及其执行时间
- * <p>
- * SQL 格式化
- * <p>
  * 分页插件
  *
  * @author xuxiaowei
+ * @since 0.0.1
  */
 @Configuration
 @EnableTransactionManagement
@@ -29,29 +22,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MybatisPlusConfig {
 
     /**
-     * SQL执行效率插件
-     * <p>
-     * 设置 dev-sql 环境开启
-     * <p>
-     * Profile({"dev-sql"})（需要在配置文件中激活）
-     */
-    @Bean
-    @Profile({"dev-sql"})
-    PerformanceInterceptor performanceInterceptor() {
-
-        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-
-        /* SQL 是否格式化 */
-        performanceInterceptor.setFormat(true);
-
-        return performanceInterceptor;
-    }
-
-    /**
      * 分页插件
+     *
+     * @return 返回 分页插件
      */
     @Bean
-    PaginationInterceptor paginationInterceptor() {
+    public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
 
